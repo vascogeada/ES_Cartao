3f5c1310b9805d6c25cd4c0a12053a413362ced1
unknown <a.roberto08@gmail.com>
2021-03-18 17:17:13 +0000

    Versão com Implementação da Classe Cartão ... sem JavaDoc

--- src/poo/Cartao.java
+++ src/poo/Cartao.java
@@ -8,49 +8,50 @@ public class Cartao {
 	
 	// Construtores 
 	public Cartao(String nomeFuncionario) {
-		
-		
+		super();
+		this.nomeFuncionario = nomeFuncionario;
 	}
 
 	public Cartao(String nomeFuncionario, String servico, String cargo) {
-		
-		
+		super();
+		this.nomeFuncionario = nomeFuncionario;
+		this.servico = servico;
+		this.cargo = cargo;
 	}
 
 	// Acessores
 	
 	public String getNomeFuncionario() {
-		
-		return "";
+		return nomeFuncionario;
 	}
 
 	public void setServico(String servico) {
-		
-		
+		this.servico = servico;
 	}
 
 	public void setCargo(String cargo) {
-		
-		
+		this.cargo = cargo;
 	}
 	
 	// Métodos
 	public void retirarServico() {
-		
-		
+		// servico = "";
+		setServico("");
 	}
 	
 	public void atribuirNovoServico( String novoServico ) {
-		
-		
+		//servico = novoServico;
+		setServico( novoServico );
 	}
 	
 	public void promoverNovoCargo( String novoCargo ) {
-		
+		//cargo = novoCargo;
+		setCargo(novoCargo);
 	}
 	
 	public void despedirFuncionario() {
-		
+		retirarServico();
+		setCargo("");
 	}
 	
 }
